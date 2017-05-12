c0536e435d1e2f317c2bff9442567e2db43272f9
lhaixiong <2100775661@qq.com>
2017-05-12 21:30:32 +0800

    Update HelloWorld.java
    edit by server

--- src/main/java/com/lhx/gitdemo/HelloWorld.java
+++ src/main/java/com/lhx/gitdemo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.lhx.gitdemo;
 public class HelloWorld {
     public void say(){
         System.out.println("hello,world");
+        System.out.println("hello,world,edited by server");
     }
 }
