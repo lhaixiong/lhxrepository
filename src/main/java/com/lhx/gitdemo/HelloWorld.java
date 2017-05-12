4bbcaa55c203f710c19f30184de0138761cc5476
lhaixiong <2100775661@qq.com>
2017-05-12 21:22:55 +0800

    提交

---
+++ src/main/java/com/lhx/gitdemo/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.lhx.gitdemo;
+
+public class HelloWorld {
+    public void say(){
+        System.out.println("hello,world");
+    }
+}
---
+++ src/test/java/com/lhx/gitdemo/Test.java
@@ -0,0 +1,4 @@
+package com.lhx.gitdemo;
+
+public class Test {
+}
